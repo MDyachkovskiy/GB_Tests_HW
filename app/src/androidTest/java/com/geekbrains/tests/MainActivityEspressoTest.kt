f1f97a7bd2972b3c0b355fe4911890bf338b5566
Mdyachkovskiy <mdyachkovskiy@gmail.com>
2023-06-26 15:36:58 +0500

    HW_5 Практика с методички Initial Test

--- app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -12,8 +12,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import com.geekbrains.tests.presenter.search.PresenterSearchContract
 import com.geekbrains.tests.view.search.MainActivity
 import org.hamcrest.CoreMatchers.not
 import org.junit.After
@@ -21,51 +19,26 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.component.KoinComponent
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.module
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest : KoinComponent {
 
     private lateinit var scenario: ActivityScenario<MainActivity>
-    private val presenter: PresenterSearchContract = mock(PresenterSearchContract::class.java)
 
     @Before
     fun setup() {
         scenario = ActivityScenario.launch(MainActivity::class.java)
-
-        val app = InstrumentationRegistry.getInstrumentation()
-            .targetContext.applicationContext as TestApp
-        app.injector = module {
-            factory<PresenterSearchContract> {presenter}
-        }
-
-        startKoin {
-            modules(app.injector)
-        }
     }
 
     @After
     fun close() {
         scenario.close()
-        stopKoin()
     }
 
     @Test
     fun testVisibilityOfTotalCountTextViewAfterDisplaySearchResults() {
         onView(withId(R.id.totalCountTextView))
             .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)))
-    }
-
-    @Test
-    fun onActivityStart_presenterOnAttachCalled() {
-        scenario.onActivity {
-            verify(presenter).onAttach(any())
-        }
     }
 
     @Test
--- app/src/androidTest/java/com/geekbrains/tests/TestApp.kt
+++
@@ -1,8 +0,0 @@
-package com.geekbrains.tests
-
-import android.app.Application
-import org.koin.core.module.Module
-
-class TestApp : Application() {
-    lateinit var injector: Module
-}
